e0a8abd24ef123f32184e727e1f5ab97e2db5d69
EthansEpic64 <hpethan64@gmail.com>
2023-10-22 21:24:20 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,6 +1,11 @@
-
+//let firstRun = true
+//let fujiimg;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  //if(firstRun){
+    //fujiimg = loadImage('mtfuji.png');
+    //firstRun = false;
+  //}
   background(240)
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
@@ -21,9 +26,27 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    fill(188, 0, 45)
    noStroke();
    ellipse (644,360, ellipsemapdrum+200, ellipsemapdrum+200);
+   if(bass >83){
+    noFill();
+    stroke(255, 255, 79);
+    strokeWeight(20);
+    beginShape();
+    vertex(200, 30);
+    vertex(220, 130);
+    vertex(250, 170);
+    vertex(226, 220);
+    vertex(294, 300);
+    vertex(270, 360);
+    vertex(310, 420);
+    vertex(280, 500);
+    vertex(349, 600);
+    endShape();
+   }
+
    
    fill(cloudColour);
    stroke(0);
+   strokeWeight(1);
    ellipse (1150, 0, ellipsemapvocal+200,ellipsemapvocal+100); //these ellipses are the clouds
    ellipse (700, 12, ellipsemapvocal+320,ellipsemapvocal+50);
    ellipse (100, 0, ellipsemapvocal+300,ellipsemapvocal+30);
@@ -34,5 +57,5 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    noStroke();
    rect (700,20,1500,45); //this just partially covers up the stroke of the clouds to make them look less like circles
    
-   
+   //image(fujiimg, 800,800);
 }
